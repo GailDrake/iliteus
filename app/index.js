af4d9d18a385d50c265c7ff5a7e37977323f9d7f
DaveSauce <dave@cumulocreative.com>
2017-08-26 08:03:09 -0400

    Basic setup

---
+++ app/index.js
@@ -0,0 +1,6 @@
+// This is the only entry point for less - no need to import less files into JS anywhere else
+import './style/less/main.less';
+
+const title = document.getElementById('home__header');
+
+title.style.color = 'pink'; // just to confirm JavaScript is working
