ea7c202c6cbe1d06793187543023e6261d961909
Harry Davidson <davidshb19@pwcs-edu.org>
2017-09-22 14:54:19 -0400

    made the defalt file system. there is a temp.txt file in every folder, you can remove once there is something else in the folder

--- app/pages/home/index.js
+++
@@ -1,8 +0,0 @@
-// This just fires on index.html
-import Handlebars from 'handlebars';
-import { renderHandlebars } from 'lib/helpers.js';
-
-console.log('index.js (home page)')
-const context = { name: 'Mickey', occupation: 'mouse' };
-
-renderHandlebars('handlebars__demo', 'handlebars__demo__target', context);
