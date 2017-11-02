2405317bc8ecb0df9753145d0d07ff8b35e1dfe3
Harry Davidson <davidshb19@pwcs-edu.org>
2017-11-02 17:45:26 -0400

    commit

--- app/index/components/navigationBox/navigationBox.js
+++ app/index/components/navigationBox/navigationBox.js
@@ -3,9 +3,7 @@ const navigationBox = {
 	body: `<div class="flex-item">
     <a href="{{item}}.html"><img class="index__navigation-box--images" src="./app/index/images/{{item}}.png"></a>
     <hr>
-    <center>
       <strong>{{item}}</strong>
-    </center>
   </div>`
 };
 
