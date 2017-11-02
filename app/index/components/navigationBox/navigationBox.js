590e9e475f236c81fed1e03eea363875a4c61c94
Harry Davidson <davidshb19@pwcs-edu.org>
2017-11-02 18:00:54 -0400

    after styles meating

--- app/index/components/navigationBox/navigationBox.js
+++ app/index/components/navigationBox/navigationBox.js
@@ -2,7 +2,6 @@ const navigationBox = {
 	name: 'box',
 	body: `<div class="flex-item">
     <a href="{{item}}.html"><img class="index__navigation-box--images" src="./app/index/images/{{item}}.png"></a>
-    <hr>
       <strong>{{item}}</strong>
   </div>`
 };
