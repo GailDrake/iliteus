f485d90b567651491c7912bd9d0cdfa4e51aeedb
Harry Davidson <davidshb19@pwcs-edu.org>
2017-11-03 15:08:26 -0400

    new tiles look

--- app/index/components/navigationBox/navigationBox.js
+++ app/index/components/navigationBox/navigationBox.js
@@ -1,8 +1,8 @@
 const navigationBox = {
 	name: 'box',
 	body: `<div class="flex-item">
-    <a href="{{item}}.html"><img class="index__navigation-box--images" src="./app/index/images/{{item}}.png"></a>
-      <strong>{{item}}</strong>
+    <a href="{{item}}.html"><img class="index__navigation-box--images" src="./app/index/images/{{item}}.png">
+      <strong>{{item}}</strong></a>
   </div>`
 };
 
