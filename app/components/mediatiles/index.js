3d3df2c7331210bcf7e7c5b7ced48cb160bc0b1b
Adeel Aziz <adeelaziz3466@gmail.com>
2017-12-14 17:03:07 -0500

    for Mr.Bascom

---
+++ app/components/mediatiles/index.js
@@ -0,0 +1,6 @@
+// This file just distributes the contents of this componentt (JS only)
+// This way, you only have to import from the directory => component/media-tiles
+
+import mediatiles from './mediatiles.js';
+
+export default mediatiles;
---
+++ app/components/mediatiles/mediatiles.js
@@ -0,0 +1,8 @@
+const mediatiles = {
+	name: 'mediatiles',
+	body: `
+		<h1>this is a test</h1>
+	`
+}
+
+export default mediatiles
