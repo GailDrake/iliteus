f47d9076a5d7318742c2276c2b25a42613fe7bfd
Adeel Aziz <adeelaziz3466@gmail.com>
2018-02-01 17:53:12 -0500

    mediatiles component using gatsby

---
+++ src/components/general/mediatiles/mediatiles.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './mediatiles.less';
+
+const Mediatiles = () => (
+  <div>
+    <p>test</p>
+  </div>
+)
+export default Mediatiles;
--- src/pages/frc/media.js
+++ src/pages/frc/media.js
@@ -1,4 +1,13 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import Mediatiles from '/Users/adeelaziz/FRC/GitHub/iliteus/src/components/general/mediatiles'
 
-export default () => <h1>This is the media page</h1>
+export default () => (
+	<div>
+
+<h1>This is the media page</h1>
+<Mediatiles />
+
+
+</div>
+)
