707d81344f8552588fac5401418b5d1308f54365
Developer5262 <dylan.fernandes00@gmail.com>
2018-02-03 15:35:15 -0500

    basic events page

--- src/pages/frc/index.js
+++ src/pages/frc/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Link from 'gatsby-link';
+import Event from '../../components/general/event';
 
 export default () =>
   <div>
     <h1>Hello Gatsby</h1>
     <p>ILITE Robotics</p>
+    <Event />
   </div>
