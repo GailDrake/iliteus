e712947d879b3b42b6acb0b9b8cab554d636d3f3
Developer5262 <dylan.fernandes00@gmail.com>
2018-02-11 18:35:50 -0500

    reconfigured less autoformatting support - waiting for restyle of events

--- src/components/events/events.js
+++ src/components/events/events.js
@@ -3,14 +3,14 @@ import FontAwesome from 'react-fontawesome';
 // import './event.less';
 
 const Events = ({ name, eventLink, date }) =>
-  <div>
+  <div className="event">
     <div>
-      <img src='http://freenaturestock.s3.amazonaws.com/1136.jpg' />
+      <img className="event__image" src='http://freenaturestock.s3.amazonaws.com/1136.jpg' />
     </div>
-    <div>
-      <a href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007">Google Map</a>
-      <a href="http://www.irocoffseason.org">{name}</a><br />
-      <div>
+    <div className="event__content">
+      <a className="event__location-link" href="https://www.google.com/maps/dir/''/battlefield+high+school/@38.8449798,-77.7005067,12z/data=!3m1!4b1!4m8!4m7!1m0!1m5!1m1!1s0x89b668be1850b531:0x54b85e4d7455f560!2m2!1d-77.6304667!2d38.8450007">Google Map</a>
+      <a className="event__link" href="http://www.irocoffseason.org">{name}</a><br />
+      <div className="event__details">
         <strong>{date}</strong><br />
         15000 Graduation Dr, Haymarket, VA 20169
       </div>
