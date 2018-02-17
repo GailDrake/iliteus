415352c84b2ce2331d57edd94420216e85865050
Nicholas Miller <nicholascmiller123@gmail.com>
2018-02-17 15:15:37 -0500

    Started tabs component

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import Link from 'gatsby-link';
 
-const Tabs = () => {
+const Tabs = ({ children, tabs }) => {
+
+  const getTabs = tabs.map((name, index) => {
+    return (
+      <li key={index}><Link to={`#tab${index}`}>{name}</Link></li>
+    );
+  });
+
   return (
     <div className="tabs">
       <ul className="tabs__head">
+        {getTabs}
       </ul>
+      <div className="tabs__content">
+        {children}
+      </div>
     </div>
   );
 }
--- src/pageLayouts/frc/Team/Team.js
+++ src/pageLayouts/frc/Team/Team.js
@@ -2,35 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import Container from '../../../templates/Container';
 import GetMember from './GetMember.js';
+import Tabs from '../../../components/Tabs';
 
+const tabs = ["Student Leaders", "Build", "Outreach", "Sponsorship", "Electronics", "Graphics", "Programming", "Web"];
 
 const Team = (props) => {
   return (
     <Container division="frc" title="Banner" subtitle="This is a banner for the team page.">
       <div className="team">
-        <li><a href="#tab1">Student Leaders</a></li>
-        <li><a href="#tab3">Build</a></li>
-        <li><a href="#tab4">Outreach</a></li>
-        <li><a href="#tab5">Sponsorship</a></li>
-        <li><a href="#tab6">Electronics</a></li>
-        <li><a href="#tab7">Graphics</a></li>
-        <li><a href="#tab8">Programming</a></li>
-        <li><a href="#tab9">Web</a></li>
-        <li><a href="#tab2">Mentors</a></li>
-
-        <br />
-
-        <div id="tab1">{GetMember("Leadership", props.teamList)}</div>
-        <div id="tab2">{GetMember("Mentors", props.teamList)}</div>
-        <div id="tab3">{GetMember("Build", props.teamList)}</div>
-        <div id="tab4">{GetMember("Outreach", props.teamList)}</div>
-        <div id="tab5">{GetMember("Sponsorship", props.teamList)}</div>
-        <div id="tab6">{GetMember("Electronics", props.teamList)}</div>
-        <div id="tab7">{GetMember("Graphics", props.teamList)}</div>
-        <div id="tab8">{GetMember("Programming", props.teamList)}</div>
-        <div id="tab9">{GetMember("Web", props.teamList)}</div>
-      </div>
-    </Container>
+        <Tabs tabs={tabs}>
+          <div id="tab1">{GetMember("Leadership", props.teamList)}</div>
+          <div id="tab2">{GetMember("Mentors", props.teamList)}</div>
+          <div id="tab3">{GetMember("Build", props.teamList)}</div>
+          <div id="tab4">{GetMember("Outreach", props.teamList)}</div>
+          <div id="tab5">{GetMember("Sponsorship", props.teamList)}</div>
+          <div id="tab6">{GetMember("Electronics", props.teamList)}</div>
+          <div id="tab7">{GetMember("Graphics", props.teamList)}</div>
+          <div id="tab8">{GetMember("Programming", props.teamList)}</div>
+          <div id="tab9">{GetMember("Web", props.teamList)}</div>
+        </Tabs>
+      </div >
+    </Container >
   );
 };
 
