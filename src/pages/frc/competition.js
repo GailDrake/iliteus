216ced67ed1ce2eac40c3717ae2f8a81df2752a0
Developer5262 <dylan.fernandes00@gmail.com>
2018-02-17 10:30:58 -0500

    edited folder structure

--- src/pages/frc/competition.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import Competition from '../../pageLayouts/frc/Competition';
-
-export default () => <Competition />
---
+++ src/pages/frc/season/calendar.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Calendar from '../../../pageLayouts/frc/Season/Calendar';
+
+export default () => <Calendar />
