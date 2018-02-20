213c56e82d00cb76a2174c0445f912f2ad464396
Developer5262 <dylan.fernandes00@gmail.com>
2018-02-20 13:25:59 -0500

    added sponsor component foundation

---
+++ src/components/Sponsor/Sponsor.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Sponsor = ({ children, name, image, url }) => {
+  return (
+    <div>
+      This is the sponsor component
+    </div>
+  );
+};
+
+export default Sponsor;
