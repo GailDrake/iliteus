6432d97a0bdaafd6c36793045679c2a733b353f4
Developer5262 <dylan.fernandes00@gmail.com>
2018-02-21 05:36:17 -0500

    added basic sponsor component and page

--- src/components/Sponsor/Sponsor.js
+++ src/components/Sponsor/Sponsor.js
@@ -1,11 +1,18 @@
 import React from 'react';
 
-const Sponsor = ({ children, name, image, url }) => {
+const Sponsor = ({ children, image, url, name }) => {
   return (
-    <div>
-      This is the sponsor component
+    <div className="sponsor">
+      <div class="sponsor__image">
+        <a href={url}>
+          <img src={image} alt={name} />
+        </a>
+      </div>
+      <div class="sponsor__text">
+        <span>{children}</span>
+      </div>
     </div>
   );
-};
+}
 
 export default Sponsor;
---
+++ src/pages/frc/sponsors.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import Sponsors from '../../pageLayouts/frc/Sponsors';
+
+export default () => <Sponsors />
