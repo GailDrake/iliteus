cccfa40ea2e3e57413a580b6baea2da0d6b66b77
Developer5262 <dylan.fernandes00@gmail.com>
2018-02-22 18:33:39 -0500

    added a few sponsors to the sponsor page

--- src/components/Sponsor/Sponsor.js
+++ src/components/Sponsor/Sponsor.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 const Sponsor = ({ children, image, url, name }) => {
+  const renderedImage = require(`../../assets/images/sponsors/${image}`);
   return (
     <div className="sponsor">
       <div className="sponsor__image-holder">
         <a href={url} className="sponsor__image">
-          <img src={image} alt={name} />
+          <img src={renderedImage} alt={name} />
         </a>
       </div>
       <div className="sponsor__text">
--- src/pageLayouts/frc/Sponsors/Sponsors.js
+++ src/pageLayouts/frc/Sponsors/Sponsors.js
@@ -6,9 +6,6 @@ const Sponsors = () => {
 	return (
 		<Container>
 			<p>This is the Sponsors page</p>
-			<Sponsor name="Temporary" url="https://dylankfernandes.github.io" image={require('../../../assets/images/frc/members/Web/Dylan F..jpg')}>
-				This is a temporary sponsors page component
-			</Sponsor>
 		</Container>
 	);
 }
