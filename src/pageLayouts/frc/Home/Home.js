96cee0180cd5657c28f010e42eea5568a72cd9d3
Developer5262 <dylan.fernandes00@gmail.com>
2018-02-26 18:47:02 -0500

    added more of the banner

--- src/pageLayouts/frc/Home/Home.js
+++ src/pageLayouts/frc/Home/Home.js
@@ -5,12 +5,17 @@ const Home = (props) => {
   return (
     <div className='home'>
       <div className="home__banner">
-        <h1 className="home__banner-title">ILITE Robotics</h1>
-        <h2 className="home__banner-subtitle">FIRST Robotics Challenge Team 1885</h2>
-        <p className="home__banner-description">FRC Robotics team based out of Haymarket, VA</p>
-        <Button division='frc' link="/about">
-          Learn More About Us!
-        </Button>
+        <div className="home__banner-text-container">
+          <h1 className="home__banner-title">ILITE Robotics</h1>
+          <h2 className="home__banner-subtitle">FIRST Robotics Challenge Team 1885</h2>
+          <p className="home__banner-description">
+            We are a team founded on the principles of leadership, teamwork, and academic rigor. Through competitions, outreach,
+            and mentorship we passionately advocate for science and technology in our schools, communities, and throughout the world.
+          </p>
+          <Button division='frc' link="/about">
+            Learn More About Us!
+          </Button>
+        </div>
       </div>
     </div>
   );
