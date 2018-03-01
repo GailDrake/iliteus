c4107649828f5b47eb0bff9289430e2eb004e199
Developer5262 <dylan.fernandes00@gmail.com>
2018-03-01 17:15:03 -0500

    added newscard to repo

--- src/components/newscard/newscard.js
+++ src/components/newscard/newscard.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import Link from 'gatsby-link';
+// import ilitelogo from '../../assets/images/logos/ilite-logo.png';
 
-const NewsCard = ({ children }) =>
-  <div><p>{children}</p></div>
-
+const NewsCard = ({image, title, text}) => {
+  console.log(image)
+  return ( 
+    <div className="news-card">
+      <div className="news-card__image-holder">
+        <img src={image} className="news-card__image"/>
+      </div>   
+      <div className="news-card__text-box">
+        <h3 className="news-card__title">{title}</h3>
+        <p className="news-card__text">{text}</p>
+      </div>
+    </div>
+  )
+}
 export default NewsCard;
