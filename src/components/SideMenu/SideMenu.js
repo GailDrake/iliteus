02619f442c8d37bf6ac3fe2d33c847f9ef2965d9
Nicholas Miller <nicholascmiller123@gmail.com>
2018-03-08 18:46:47 -0500

    Side bar transition finished

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -1,17 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'gatsby-link';
 
-const SideMenu = () => {
-  return (
-    <div className="sidemenu">
-      <ul className="sidemenu__link-list">
-        <li>Robotics</li>
-        <li>Cyber</li>
-        <li>Computer Science</li>
-        <li>Hosted Events</li>
-      </ul>
-    </div>
-  );
+class SideMenu extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      sidemenu: false,
+    };
+  }
+
+  openSideMenu = () => {
+    this.setState({
+      sidemenu: true
+    })
+  }
+
+  closeSideMenu = () => {
+    this.setState({
+      sidemenu: false
+    })
+  }
+
+  render() {
+    const sidemenu = this.state.sidemenu ? "250px" : "0px";
+
+    return (
+      <div className="sidemenu" >
+        <span className="sidemenu__burger">
+          <a href="#" onClick={() => this.openSideMenu()}>
+            <svg width="30" height="30">
+              <path d="M0,5 30,5" stroke="#000" strokeWidth="5" />
+              <path d="M0,14 30,14" stroke="#000" strokeWidth="5" />
+              <path d="M0,23 30,23" stroke="#000" strokeWidth="5" />
+            </svg>
+          </a>
+        </span>
+
+        <div id="sidemenu__menu" className="sidemenu__nav" style={{ width: sidemenu }}>
+          <a href="#" className="sidemenu__close" onClick={() => this.closeSideMenu()}>&times;</a>
+          <a href="#">FRC</a>
+          <a href="#">FTC</a>
+          <a href="#">Hosted Events</a>
+          <a href="#">Outreach</a>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default SideMenu;
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -5,21 +5,10 @@ import SideMenu from '../SideMenu';
 class Navbar extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      clicked: false,
-    };
-  }
-
-  toggle = () => {
-    const value = this.state.clicked ? false : true;
-    this.setState({
-      clicked: value
-    })
   }
 
   render() {
     const { links } = this.props;
-    const sidemenu = this.state.clicked === true ? <SideMenu id="navigation__sidemenu" /> : "";
     const getLinks = links.map((link, index) => {
       let filePath = `/${link.section}/${link.file}`;
       return (
@@ -33,8 +22,9 @@ class Navbar extends Component {
         <ul>
           {getLinks}
         </ul>
-        <div className={`navigation__burger ${this.state.clicked}`} onClick={this.toggle}></div>
-        {sidemenu}
+        <div className="navigation__sidemenu">
+          <SideMenu />
+        </div>
       </nav >
     );
   }
