db9b1ae15e1ee03ae27aeb6253593558db8e9460
Developer5262 <dylan.fernandes00@gmail.com>
2018-03-24 16:04:00 -0400

    finished competition page styles

--- src/pageLayouts/frc/Competition/Competition.js
+++ src/pageLayouts/frc/Competition/Competition.js
@@ -20,6 +20,34 @@ const Competition = () => {
           <li><a href="#tab2">2007</a></li>
           <li><a href="#tab1">2006</a></li>
         </div>
+        <div className="competition__event" id="tab12">
+          <h1 className="competition__event-header">2017 - Steamworks</h1>
+          <iframe
+            width="100%"
+            height="315"
+            src="https://www.youtube.com/embed/MM_JWAON6PI"
+            frameborder="0"
+            gesture="media"
+            allow="encrypted-media"
+            allowfullscreen />
+          <div className="competition__event-container">
+            <div>
+              <h3 className="competition__event-subheader">Awards</h3>
+              <ul>
+                <li>FIRST World Championship: Chairman's Award Finalist</li>
+                <li>CHS District Championship: Regional Chairman's Award</li>
+                <li>CHS District Championship: District Championship Finalist</li>
+                <li>CHS Greater DC District: District Chairman's Award</li>
+                <li>CHS Greater DC District: District Event Winner</li>
+                <li>CHS Central Virginia District: District Event Winner</li>
+                <li>CHS Central Virginia District: Industrial Design Award</li>
+              </ul>
+            </div>
+            <div>
+              <h3 className="competition__event-subheader">Competitions Attended</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </Container>
   );
