9a2f71cc37a284714bffa870629008811bf7c4d6
Nicholas Miller <nicholascmiller123@gmail.com>
2018-03-24 20:17:31 -0400

    Made style changes and JavaScript improvements to sidebar. Also fixed navbar scroll issue

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -6,9 +6,6 @@ class SideMenu extends Component {
     super(props);
     this.state = {
       sidemenu: false,
-      dropdown1: false,
-      dropdown2: false,
-      dropdown3: false,
     };
   }
 
@@ -24,16 +21,9 @@ class SideMenu extends Component {
     })
   }
 
-  dropDown1 = () => {
-    const toggle = this.state.dropdown1 ? false : true;
-    this.setState({
-      dropdown1: toggle
-    })
-  }
-
   render() {
-    const sidemenu = this.state.sidemenu ? "250px" : "0px";
-    const dropdown1 = this.state.dropdown1 ? "279px" : "0px";
+    const sidemenu = this.state.sidemenu ? "translateX(-250px)" : "translateX(0px)";
+    const background = this.state.sidemenu ? "block" : "none";
 
     return (
       <div className="sidemenu" >
@@ -47,33 +37,33 @@ class SideMenu extends Component {
           </div>
         </span>
 
-        <div className="sidemenu__nav" style={{ width: sidemenu }}>
+        <div className="sidemenu__background" onClick={() => this.closeSideMenu()} style={{ display: background }}></div>
+        <div className="sidemenu__nav" style={{ transform: sidemenu }}>
           <span className="sidemenu__close" onClick={() => this.closeSideMenu()}>&times;</span>
           <div className="sidemenu__list">
-            <div className="sidemenu__droptitle" onClick={() => this.dropDown1()}>
-              <span className="sidemenu--title">FRC Robotics</span><span className="sidemenu--arrow">&#9660;</span>
-            </div>
-            <div className="sidemenu__dropcontent" style={{ height: dropdown1 }}>
+            <Link onClick={() => this.closeSideMenu()} to="/frc" className="sidemenu--link title">FRC Robotics</Link>
+            <div className="sidemenu__sublinks">
               <Link onClick={() => this.closeSideMenu()} to="/frc">Home</Link>
+              <br />
               <Link onClick={() => this.closeSideMenu()} to="/frc/about">About</Link>
+              <br />
               <Link onClick={() => this.closeSideMenu()} to="/frc/media">Media</Link>
+              <br />
               <Link onClick={() => this.closeSideMenu()} to="/frc/team">Team</Link>
+              <br />
               <Link onClick={() => this.closeSideMenu()} to="/frc/competition">Competition</Link>
+              <br />
               <Link onClick={() => this.closeSideMenu()} to="/frc/getinvolved">Get Involved</Link>
             </div>
             <br />
             <br />
-            <div className="sidemenu__droptitle">
-              <a href="http://irocoffseason.org/" onClick={() => this.closeSideMenu()}>IROC</a>
-            </div>
+            <a href="http://irocoffseason.org/" onClick={() => this.closeSideMenu()} className="sidemenu--link">IROC</a>
             <br />
             <br />
-            <div className="sidemenu__droptitle">
-              <a href="http://www.haymakerstemexpo.org/" onClick={() => this.closeSideMenu()}>STEAM Expo</a>
-            </div>
+            <a href="http://www.haymakerstemexpo.org/" onClick={() => this.closeSideMenu()} className="sidemenu--link">STEAM Expo</a>
           </div>
         </div>
-      </div >
+      </div>
     );
   }
 }
