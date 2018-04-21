f3dc3045ef7f2a817de6a8395deb9492c214f3da
Kyle Rowland <rowlandloud@gmail.com>
2018-04-21 14:36:24 -0400

    media

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -4,11 +4,8 @@ import Link from 'gatsby-link';
 
 const Button = ({ division, children, link }) => {
   const isExternalLink = link.substr(0, 3) === 'http';
-  // const location = link.substr(0, 4) == 'http'
-  //   ? <a href={link}>{children}</a>
-  //   : <Link to={link}>{children}</Link>
   return ( 
-   //<div className={`button ${division}`}>{isExternalLink}</div> 
+  
   <div>
     {
     isExternalLink 
