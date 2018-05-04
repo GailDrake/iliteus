a7f1f0d2b1e80dce51f5ace5460d8201ac6a5012
Developer5262 <dylan.fernandes00@gmail.com>
2018-05-04 18:11:29 -0400

    fixed container and merge request conflicts

--- src/pageLayouts/frc/Team/Team.js
+++ src/pageLayouts/frc/Team/Team.js
@@ -8,11 +8,7 @@ import QuoteRight from '../../../IconComponents/Quoteright';
 
 const Team = ({ teamList }) => {
   return (
-<<<<<<< HEAD
     <Container title="Banner" subtitle="This is a banner for the team page.">
-=======
-    <Container division="frc" title="Team Organization & Members" subtitle="ILITE has a team structure that emphasises student leadership and mentorship.">
->>>>>>> master
       <div className="team">
         <Tabs>
           <Tab name="Leadership">
