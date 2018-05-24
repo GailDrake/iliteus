00e3abf376b767b6322c331e87ce8c234530389e
Developer5262 <dylan.fernandes00@gmail.com>
2018-05-24 18:02:46 -0400

    fixed requested changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -9,29 +9,27 @@ class Navbar extends Component {
 
     render() {
         const { links } = this.props;
-        const getLinks = links.map((link, index) => {
-            console.log(link.url);
-
-            return link.url.substring(0, 4) === 'http'
-                ?
-                <li key={index}>
-                    <a href={link.url} className="navigation__list-item">
-                        {link.name}
-                    </a>
-                </li>
-                :
-                <li key={index}>
-                    <Link to={link.url} className="navigation__list-item">
-                        {link.name}
-                    </Link>
-                </li>
-        });
-
         return (
             <nav className="navigation">
                 <Link to="/frc" className="navigation__logo"></Link>
                 <ul>
-                    {getLinks}
+                    {links.map((link, index) => {
+                        console.log(link.url);
+
+                        return link.url.substring(0, 4) === 'http'
+                            ?
+                            <li key={index}>
+                                <a href={link.url} className="navigation__list-item">
+                                    {link.name}
+                                </a>
+                            </li>
+                            :
+                            <li key={index}>
+                                <Link to={link.url} className="navigation__list-item">
+                                    {link.name}
+                                </Link>
+                            </li>
+                    })}
                 </ul>
                 <div className="navigation__sidemenu">
                     <SideMenu />
