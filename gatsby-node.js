31d3da44c73c071b14081c7936c3d08ab5d1c5ad
Developer5262 <dylan.fernandes00@gmail.com>
2018-05-31 15:56:20 -0400

    finished ilite routing and made hosted events page reponsive

--- gatsby-node.js
+++ gatsby-node.js
@@ -4,18 +4,17 @@ exports.onCreatePage = async ({ page, boundActionCreators }) => {
   const { createPage } = boundActionCreators;
 
   return new Promise((resolve, reject) => {
+    page.layout = "frc";
     if (page.path.match(/^\/frc/)) {
       page.layout = "frc";
-      createPage(page);
     }
     else if (page.path.match(/^\/outreach/)) {
       page.layout = "outreach";
-      createPage(page);
     }
     else if (page.path.match(/^\/hosted-events/)) {
       page.layout = "hosted-events";
-      createPage(page);
     }
+    createPage(page);
     resolve();
   });
 };
