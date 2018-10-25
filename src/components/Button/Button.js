d8fb8404336219ee9337ed6ccb751d56a0647b3b
Upneet <captainus55@gmail.com>
2018-10-25 14:36:52 -0400

    Made entire button a link for all buttons on site

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,10 +2,10 @@ import React from 'react';
 import Link from 'gatsby-link';
 
 const Button = ({ division, children, link }) => {
-  const location = link.substr(0, 4) == 'http'
-    ? <a href={link}>{children}</a>
-    : <Link to={link}>{children}</Link>
-  return <div className={`button ${division}`}>{location}</div>
+  const location = (link.substr(0, 4) == 'http')
+    ? <a href={link} className={`button ${division}`}>{children}</a>
+    : <Link to={link} className={`button ${division}`}>{children}</Link>;
+  return location;/*<div className={`button ${division}`}>{location}</div>*/
 }
 
 export default Button;
