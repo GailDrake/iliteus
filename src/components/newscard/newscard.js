afa76f744985b73a51bb6c2136b6c9e24c6ec34e
OttCS20 <OttCS20@pwcs-edu.org>
2018-11-01 16:08:28 -0400

    animations removed

--- src/components/newscard/newscard.js
+++ src/components/newscard/newscard.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Link from 'gatsby-link';
-import Fade from 'react-reveal';
 
 const NewsCard = ({ image, title, children }) => {
   return (
-    <Fade bottom>
       <div className="news-card">
         <div className="news-card__image-holder">
           <img src={image} className="news-card__image" />
@@ -14,7 +12,6 @@ const NewsCard = ({ image, title, children }) => {
           <p className="news-card__text">{children}</p>
         </div>
       </div>
-    </Fade>
   )
 }
 export default NewsCard;
