f819f9141bdae1a3465c3ad74419256d44b6081f
Nicholas Miller <nicholascmiller123@gmail.com>
2019-01-03 19:07:33 -0500

    added javascript to detect current section

--- src/components/overbar/overbar.js
+++ src/components/overbar/overbar.js
@@ -1,17 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Link from 'gatsby-link';
 
-const Overbar = ({ theme }) => {
-  return (
-    <nav>
-      <ul className={`overbar__list ${theme}`}>
-        <li><Link to="/frc" className="overbar__list-item">FRC Robotics</Link></li>
-        <li><a href="http://irocoffseason.org" className="overbar__list-item">IROC Offseason</a></li>
-        <li><a href="http://www.ilite.info" className="overbar__list-item">STEAM Expo</a></li>
-        <li><Link to="/outreach" className="overbar__list-item">Outreach</Link></li>
-      </ul>
-    </nav>
-  )
+class Overbar extends Component {
+  constructor(props) {
+    super(props);
+    this.ilite = "";
+    this.frc = "";
+    this.outreach = "";
+  }
+
+  sectionCheck = (url) => {
+    if (url.includes("ilite")) {
+      this.ilite = "overbar--underlined";
+      this.frc = "";
+      this.outreach = "";
+    }
+    else if (url.includes("frc")) {
+      this.ilite = "";
+      this.frc = "overbar--underlined";
+      this.outreach = "";
+    }
+    else if (url.includes("outreach")) {
+      this.ilite = "";
+      this.frc = "";
+      this.outreach = "overbar--underlined";
+    }
+  }
+
+  render() {
+    var url = window.location.href;
+    this.sectionCheck(url);
+    console.log("frc:" + this.frc);
+    console.log("ilite:" + this.ilite);
+    console.log("outreach:" + this.outreach);
+
+    return (
+      <nav>
+        <ul className="overbar__list frc">
+          <li><Link to="/ilite" className={`overbar__list-item ${this.ilite}`}>About ILITE</Link></li>
+          <li><Link to="/frc" className={`overbar__list-item ${this.frc}`}>FRC Robotics</Link></li>
+          <li><a href="http://www.ilite.info" className="overbar__list-item">STEAM Expo</a></li>
+          <li><Link to="/outreach" className={`overbar__list-item ${this.outreach}`}>Outreach</Link></li>
+        </ul>
+      </nav>
+    );
+  }
 }
 
 export default Overbar;
