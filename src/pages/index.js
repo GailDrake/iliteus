eb1746459a998cbd5bbce8aaa76042e07a834286
Nicholas Miller <nicholascmiller123@gmail.com>
2019-01-06 11:21:00 -0500

    Made pages/index.js redirect to About ILITE home

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,57 +1,10 @@
-import React from "react";
-import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import Navbar from "../components/Navbar";
-import Overbar from "../components/overbar";
-import Footer from "../components/footer";
-import Favi from "../assets/icons/favicon.ico";
-import IliteHome from "../pageLayouts/ilite/IliteHome";
+import React from 'react';
+import { Redirect } from 'react-router';
 
-// import less here - once, for the whole app
-import "./../config/main.less";
-
-const links = [
-  {
-    name: "Get Involved",
-    section: "ilite",
-    file: "getinvolved"
-  },
-  {
-    name: "Sponsors",
-    section: "ilite",
-    file: "sponsors"
-  },
-  {
-    name: "Home",
-    section: "ilite",
-    file: ""
+class HomeRedirect extends React.Component {
+  render() {
+    return <Redirect to='/ilite' />;
   }
-];
+}
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="ILITE"
-      meta={[
-        { name: "description", content: "ILITE" },
-        {
-          name: "keywords",
-          content: "Inspring Leaders in Technology and Engineering"
-        },
-        { name: "viewport", content: "width=device-width" }
-      ]}
-      link={[{ rel: "shortcut icon", type: "image/png", href: `${Favi}` }]}
-    />
-
-    <Overbar theme="frc" />
-    <Navbar links={links} />
-    <IliteHome />
-    <Footer />
-  </div>
-);
-
-TemplateWrapper.propTypes = {
-  children: PropTypes.func
-};
-
-export default TemplateWrapper;
+export default HomeRedirect;
