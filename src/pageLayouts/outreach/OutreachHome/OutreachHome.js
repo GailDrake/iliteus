6fc47fd7f1a3ad60d2a66b64d8d68355ccf3e395
OttCS20 <OttCS20@pwcs-edu.org>
2019-01-13 15:19:30 -0500

    Proper event cards put under each category, not completed

--- src/pageLayouts/outreach/OutreachHome/OutreachHome.js
+++ src/pageLayouts/outreach/OutreachHome/OutreachHome.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import Events from '../../../components/events';
-import SponsorTag from '../../../components/SponsorTag';
-import NewsCard from '../../../components/newscard';
 import Button from '../../../components/Button';
 
 const OutreachHome = (props) => {
@@ -17,26 +15,51 @@ const OutreachHome = (props) => {
           </Button>
         </div>
       </div>
-      <div className="outreachHome__events">
-        <h1 className="outreachHome__events-header">About Outreach</h1>
+      <div className="outreachHome__description">
+        <h1 className="outreachHome__description-header">About Outreach</h1>
         <br />
-        <p className="outreachHome__description">
+        <p className="outreachHome__description-text">
         ILITE hosts and plans a majority of the STEAM related events at our school. Additionally, we help spread the word, and bring STEAM to our community in a big way. We utilize events such as the STEAM Expo, and the Summer Camps to teach our community about the different aspects of STEAM, as well as get them interested in the possibilities STEAM has for the future. Our goal as ILITE is to inspire the future generation and encourage them to pursue STEAM.
         </p>
       </div>
-      <div id="news" className="outreachHome__news">
-        <h1 className="outreachHome__news-header">Upcoming Events</h1>
-        <br />
-        <p className="outreachHome__description">
-        We host a multitude of events including the STEAM Expo, the FRC Competition, and the FTC competition. Click on the images below to find out more.
-        </p>
-      </div>
-      <div className="outreachHome__sponsors">
-        <h1 className="outreachHome__sponsors-header">FLL and Summer Camps</h1>
-        <br />
+      <div className="outreachHome__events">
+        <h1 className="outreachHome__events-header">Upcoming Events</h1>
         <p className="outreachHome__description">
         We host seasonal activities teaching kids about certain aspects of STEAM and robotics with FLL being in the fall, and the summer camps being in the summer. In doing this, we introduce the kids to the world of STEAM, and nurture their interest in technology.
         </p>
+        <div className="outreachHome__events-container">
+          <Events
+            name="Steam Expo"
+            eventLink="http://www.ilite.info/"
+            image="images/outreach/steamexpo/expo.jpg" />
+          
+          <Events
+            name="FRC Competitions"
+            eventLink="frc/competition"
+            image="images/frc/district_chairmans.jpeg" />
+
+          <Events
+            name="FTC Robotics"
+            eventLink="https://www.irocoffseason.org"
+            image="images/FTC_logo.png" />
+        </div>
+      </div>
+      <div className="outreachHome__sponsors">
+        <h1 className="outreachHome__sponsors-header">FLL and Summer Camps</h1>
+        <p className="outreachHome__description">
+        We host seasonal activities teaching kids about certain aspects of STEAM and robotics with FLL being in the fall, and the summer camps being in the summer. In doing this, we introduce the kids to the world of STEAM, and nurture their interest in technology.
+        </p>
+        <div className="outreachHome__events-container">
+        <Events
+            name="FLL Workshops"
+            eventLink="outreach/Fll"
+            image="images/outreach/fll/fairoaks.png" />
+
+          <Events
+            name="Summer Camps"
+            eventLink="outreach/SummerCamps"
+            image="images/outreach/summercamps/stem1.jpg" />
+        </div>
       </div>
     </div>
   );
