a2da8be33bf7257d6e2306110765ede0acd9cc93
dmbjm <benzeitlin1010@gmail.com>
2019-11-14 21:06:01 -0500

    Added data

--- src/pageLayouts/outreach/Documentation/Documentation.js
+++ src/pageLayouts/outreach/Documentation/Documentation.js
@@ -1,12 +1,682 @@
 import React from "react";
 import Container from '../../../templates/container';
+import Tabs, { Tab } from '../../../components/Tabs';
 
 const Documentation = () => {
   return (
     <Container title="Documentation" subtitle="Information about everything Robotics">
-      <div className="documentation">
-
+      <div className="documentation_overview">
+        <div className="documentation_overview-description">
+          <p className="documentation_overview__text">
+            Welcome to our Documentation! Here, you can find past events and information
+            relating to our team. EDIT
+          </p>
+        </div>
       </div>
+      <div className="documentation_offical">
+        <Tabs>
+          <Tab name="2014-2015">
+            <table>
+              <tr>
+                <th>Event Name</th>
+                <th>Minds Reached</th>
+                <th>Links</th>
+              </tr>
+              <tr>
+                <td>Sky Robotics</td>
+                <td>42</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymarket Day</td>
+                <td>300</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Superintendent Breakfast</td>
+                <td>110</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Summer Camps 2014</td>
+                <td>190</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>DC United</td>
+                <td>800</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>IROC 2014</td>
+                <td>750</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Orchestra</td>
+                <td>29</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Governor's School</td>
+                <td>20</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Air and Scare 2014</td>
+                <td>1000</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FLL Regional</td>
+                <td>400</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>STEM Expo</td>
+                <td>60</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Gravely Tutoring</td>
+                <td>480</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymarket Lifestyle Magazine</td>
+                <td></td>
+                <td></td>
+              </tr>
+            </table>
+          </Tab>
+          <Tab name="2015-2016">
+            <table>
+              <tr>
+                <th>2015-2016</th>
+              </tr>
+              <tr>
+                <th>Event Name</th>
+                <th>Minds Reached</th>
+                <th>Links</th>
+              </tr>
+              <tr>
+                <td>Haiti, Ethiopia, Malaysia</td>
+                <td>50</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Summer Camps 2015</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymarket Day 2015</td>
+                <td>400</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymarker STEM Expo</td>
+                <td>2000</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymarket Lifesyle Magazine</td>
+                <td>1500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Hosted FRC District</td>
+                <td>2500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>IROC 2015</td>
+                <td>750</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC</td>
+                <td>30</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Cyber Program</td>
+                <td>86</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Cyber Class</td>
+                <td>29</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Sustainability</td>
+                <td>32</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Judging Science Fairs</td>
+                <td>20</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Prince William Living Magazine</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Valley Voice (Spring 16, Winter 16)</td>
+                <td></td>
+                <td></td>
+              </tr>
+            </table>
+          </Tab>
+          <Tab name="2016-2017">
+            <table>
+              <tr>
+                <th>2016-2017</th>
+              </tr>
+              <tr>
+                <th>Event Name</th>
+                <th>Minds Reached</th>
+                <th>Links</th>
+              </tr>
+              <tr>
+                <td>Summer Camps 2016</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymarket Day 2016</td>
+                <td>400</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>IROC 2016</td>
+                <td>750</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC 2016</td>
+                <td>800</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Alvey Elementary FLL</td>
+                <td>20</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Cyber Program</td>
+                <td>63</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC Program</td>
+                <td>30</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Youth for Tomorrow</td>
+                <td>300</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Walk for Autism</td>
+                <td>450</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Payton's Walk</td>
+                <td>450</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>DC Unied</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Alvey Summer Camps Table</td>
+                <td>20</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymaker STEAM Expo</td>
+                <td>2032</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Reagan MS Science Fair</td>
+                <td>150</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Gravely Elementary Science Fair</td>
+                <td>50</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>NOVEC Meeting</td>
+                <td>100</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FRC District 2017</td>
+                <td>2500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Leadership Conference (FIRST CMP 2016)</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Elderly Home</td>
+                <td>15</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Girls in Aviation Day</td>
+                <td>98</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Kindness Campaign</td>
+                <td>30</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Valley Voice (Fall 16, Summer 16)</td>
+                <td></td>
+                <td></td>
+              </tr>
+            </table>
+          </Tab>
+          <Tab name="2017-2018">
+            <table>
+              <tr>
+                <th>2017-2018</th>
+              </tr>
+              <tr>
+                <th>Event Name</th>
+                <th>Minds Reached</th>
+                <th>Links</th>
+              </tr>
+              <tr>
+                <td>Summer Camps 2017</td>
+                <td>155</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Payton's Walk</td>
+                <td>100</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Girls in Aviation Day</td>
+                <td>63</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymaker STEAM Expo</td>
+                <td>2076</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Displaced Military Families</td>
+                <td>15</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC hosting Competition</td>
+                <td>500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Jamaica FLL Kits</td>
+                <td>12</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Alvey ES FLL</td>
+                <td>12</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FLL Workshops</td>
+                <td>57</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Cyber Program</td>
+                <td>47</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC Program</td>
+                <td>35</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>NOVEC Meeting</td>
+                <td>60</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Superintendent Breakfast</td>
+                <td>120</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Freshman Orientation</td>
+                <td>900</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Walk for Autism</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>IROC hosting 2017</td>
+                <td>750</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Pauton's Walk</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Gravely Pi Night</td>
+                <td>300</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>DaVinchi Robot</td>
+                <td>6</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FRC District 2018</td>
+                <td>1500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>AFCEA Luncheon</td>
+                <td>60</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>U.S. Science & Tech Fair</td>
+                <td></td> {/*Figure out the amount*/}
+                <td></td>
+              </tr>
+              <tr>
+                <td>Mentor Celebration at District Champs MD</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Mountain View Code Night</td>
+                <td>350</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Reagan Tech Night</td>
+                <td>50</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Daycare in Manassas</td>
+                <td>110</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Leadership Confrerence (FIRST CMP 2017)</td>
+                <td>400</td>
+                <td></td>
+              </tr>
+            </table>
+          </Tab>
+          <Tab name="2018-2019">
+            <table>
+              <tr>
+                <th>2018-2019</th>
+              </tr>
+              <tr>
+                <th>Event Name</th>
+                <th>Minds Reached</th>
+                <th>Links</th>
+              </tr>
+              <tr>
+                <td>Summer Camps 2018</td>
+                <td>223</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Girls in Aviation Day</td>
+                <td>63</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>PWC Haymaker STEAM Expo</td>
+                <td>2339</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC hosting Competition</td>
+                <td>500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FLL Workshops</td>
+                <td>65</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Cyber Program</td>
+                <td>68</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC Program</td>
+                <td>50</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>NOVEC Meeting</td>
+                <td>300</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Freshman Orientation</td>
+                <td>900</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>IROC hosting Oct 2018</td>
+                <td>750</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Compass Pre-School</td>
+                <td>125</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Battlefield's clubs meetings</td>
+                <td>35</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Association of Old Crows</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Malaysia</td>
+                <td>40</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Haymarket Day</td>
+                <td>500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Kickoff</td>
+                <td>127</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC X bots 6700</td>
+                <td>2</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>1731 Fresta Valley</td>
+                <td>17</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>George Mason High School strategy</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Girl scout outreach</td>
+                <td>100</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FRC District 2019</td>
+                <td>1500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Mentor Celebration at District Champs</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Leadership Conference (FIRST CMP 2018)</td>
+                <td>400</td>
+                <td></td>
+              </tr>
+            </table>
+          </Tab>
+          <Tab name="2019-2020">
+            <table>
+              <tr>
+                <th>2018-2019</th>
+              </tr>
+              <tr>
+                <th>Event Name</th>
+                <th>Minds Reached</th>
+                <th>Links</th>
+              </tr>
+              <tr>
+                <td>Summer Camps 2019</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Girls in Aviation Day</td>
+                <td>63</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>PWC Haymaker STEAM Expo</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Cyber Program</td>
+                <td>68</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FTC Program</td>
+                <td>50</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>NOVEC Meeting</td>
+                <td>300</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Freshman Orientation</td>
+                <td>900</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>IROC hosting Oct 2019</td>
+                <td>750</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Battlefield's clubs meetings</td>
+                <td>35</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Association of Old Crows</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Girl Scout Programming Lessons</td>
+                <td>18</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Spirit Bot Football Games</td>
+                <td>900</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FLL Elementary School Mentoring</td>
+                <td>40</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>FRC District 2020</td>
+                <td>1500</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Mentor Celebration at District Champs</td>
+                <td>200</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Leadership Conference (FIRST CMP 2020)</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Malaysia</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Kickoff</td>
+                <td></td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Girl scout Presentation to Leaders</td>
+                <td></td>
+                <td></td>
+              </tr>
+            </table>
+          </Tab>
+        </Tabs>
+      </div>
+
     </Container>
   );
 };
