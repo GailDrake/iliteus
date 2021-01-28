f462e00136b3d3e774fc88f6da15552f704ea236
Agrim Sharma <76flyershd@gmail.com>
2021-01-27 20:45:51 -0500

    final changes, awaiting PR

--- src/pageLayouts/outreach/HostedEvents/HostedEvents.js
+++ src/pageLayouts/outreach/HostedEvents/HostedEvents.js
@@ -10,8 +10,9 @@ const HostedEvents = () => (
       subtitle="Check out all of the events hosted by ILITE"
       bannerImage="hostedevents__banner"
     >
-      <h1>2020 - 2021 Virtual Season</h1>
-      <hr className="hostedevents__hr" />
+      <div className="hosted-events">
+        <h1 className = "hostedevents__header">2020 - 2021 Virtual Season</h1>
+        <hr className="hostedevents__hr" />
 
           <div className="event">
             <div className="event__description">
@@ -19,6 +20,7 @@ const HostedEvents = () => (
                 <img
                   src={require("../../../assets/images/outreach/catapultcompetition/imagetwo.jpeg")}
                   alt="Catapult Competition"
+                  style = {{height: "300px"}}
                 />
               </div>
               <div>
@@ -29,9 +31,11 @@ const HostedEvents = () => (
                   own event in house. Teams were tasked with building their own catapult. At the event, the team that launched the designated object
                   the farthest won.
                 </p>
+                <br/>
               </div>
             </div>
           </div>
+      </div>
       
       <div className="hosted-events">
         <h1 className="hostedevents__header">Other events</h1>
@@ -175,28 +179,7 @@ const HostedEvents = () => (
             </div>
           </div>
 
-          <hr className="hostedevents__hr" />
-
-          <div className="event">
-            <div className="event__description">
-              <div>
-                <img
-                  src={require("../../../assets/images/outreach/catapultcompetition/imagetwo.jpeg")}
-                  alt="Catapult Competition"
-                />
-              </div>
-              <div>
-                <h1 className="hostedevents__subheader">Catapult Competition</h1>
-                <hr />
-                <p>
-                  Due to the COVID-19 Pandemic, ILITE was unable to do any offseason events. That being said, ILITE decided to make their 
-                  own event in house. Teams were tasked with building their own catapult. At the event, the team that launched the designated object
-                  the farthest won.
-                </p>
-              </div>
-            </div>
-          </div>
-
+          
           {/*<hr className="hostedevents__hr" />
 
       <div className="event">
