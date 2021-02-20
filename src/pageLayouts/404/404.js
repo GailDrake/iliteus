6e893c218f6b34bf31c59d5fc29968ffb6e19ab8
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2021-02-20 10:43:59 -0500

    Page complete, still need to fix CSS and Banner

---
+++ src/pageLayouts/404/404.js
@@ -0,0 +1,41 @@
+import React from "react"
+import LayoutWrapper from "../../components/LayoutWrapper"
+import Container from "../../templates/container"
+import Button from "../../components/Button"
+
+const NotFoundPage = () => {
+    return (
+        <LayoutWrapper>
+            <Container
+                title="Page Not Found"
+                subtitle="The page you attempted to visit was either moved or does not exist"
+                bannerImage="get__involved_banner"
+            >
+                <div className="page-not-found"> 
+                    <h1 className="page-not-found_hr">Please refer to the top right of the page to relocate to the correct page</h1>
+                    <p className="page-not-found_p">
+                        Here are some other links that may be helpful
+                        <ul>
+                                <Button
+                                    link="https://www.firstinspires.org/"
+                                    division="frc"
+                                    className="page-not-found_button"
+                                >
+                                    First Inspires
+                                </Button>
+                                <Button
+                                    link="https://www.firstchesapeake.org/first-programs/frc"
+                                    division="frc"
+                                    className="page-not-found_button"
+                                >
+                                    First Chesapeake
+                                </Button>
+                        </ul>
+                    </p>
+                </div>
+            </Container>
+        </LayoutWrapper>
+    )
+}
+
+export default NotFoundPage
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,38 +1,4 @@
-import React from "react"
+import React from 'react';
+import NotFoundPage from '../pageLayouts/404';
 
-import Layout from "../components/layout"
-import SEO from "../components/seo"
-
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <div className="Page_Not_Found"> 
-      <h1>Page Not Found</h1>
-      <br />
-      <h2>The page you attempted to visit either does not exist or was moved!</h2>
-    </div>
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-    <br />
-  </Layout>
-)
-
-export default NotFoundPage
+export default () => <NotFoundPage />
