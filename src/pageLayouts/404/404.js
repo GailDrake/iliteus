ccd2c2c7310214aaf4872cc3e1a14ee786b51741
Ibrahim Abbasi <Ibrahimzabbasi04@gmail.com>
2021-02-20 12:02:37 -0500

    Fixed Issue with Banner

--- src/pageLayouts/404/404.js
+++ src/pageLayouts/404/404.js
@@ -9,24 +9,24 @@ const NotFoundPage = () => {
             <Container
                 title="Page Not Found"
                 subtitle="The page you attempted to visit was either moved or does not exist"
-                bannerImage="get__involved_banner"
+                bannerImage="not__found_banner"
             >
                 <div className="page-not-found"> 
-                    <h1 className="page-not-found_hr">Please refer to the top right of the page to relocate to the correct page</h1>
-                    <p className="page-not-found_p">
+                    <h1 className="page-not-found__header">Please refer to the top right of the page to relocate to the correct page</h1>
+                    <p className="page-not-found__p">
                         Here are some other links that may be helpful
                         <ul>
                                 <Button
                                     link="https://www.firstinspires.org/"
                                     division="frc"
-                                    className="page-not-found_button"
+                                    className="page-not-found__button"
                                 >
                                     First Inspires
                                 </Button>
                                 <Button
                                     link="https://www.firstchesapeake.org/first-programs/frc"
                                     division="frc"
-                                    className="page-not-found_button"
+                                    className="page-not-found__button"
                                 >
                                     First Chesapeake
                                 </Button>
