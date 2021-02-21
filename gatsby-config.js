c3a45a6042d5d40cda7c82ace0f73921c94cb709
Agrim Sharma <76flyershd@gmail.com>
2021-02-21 16:38:21 -0500

    google analytics added to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,6 +5,14 @@ module.exports = {
     author: `@gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-134138489-1", 
+        head: true,
+        anonymize: true,
+      },
+    },
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-source-filesystem`,
